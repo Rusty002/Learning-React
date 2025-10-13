61cd1a44a8605ef1a1330980899330d0192fa527
abdurRehman <Administrator@DESKTOP-GARLJAI>
2025-10-13 22:53:34 +0500

    calculator project added

--- projects/3-todo-app-version-2/src/components/TodoItems.tsx
+++ projects/3-todo-app-version-2/src/components/TodoItems.tsx
@@ -1,14 +1,13 @@
 import TodoItem from "./TodoItem";
+import styles from './TodoItems.module.css';
 
 function TodoItems({ todoItems }: any) {
   return (
-    <>
-      <div className="items-container">
+      <div className={styles.itemsContainer}>
         {todoItems.map((item: any) => (
           <TodoItem todoName={item.name} todoDate={item.dueDate}></TodoItem>
         ))}
       </div>
-    </>
   );
 }
 

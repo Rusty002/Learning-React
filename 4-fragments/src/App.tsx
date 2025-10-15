982210f839dc95f09f1f6f27577cf37cf4af700f
Abdur Rehman <a.amjad@seersolutionz.com>
2025-10-15 11:32:06 +0400

    state management learning

--- 4-fragments/src/App.tsx
+++ 4-fragments/src/App.tsx
@@ -3,28 +3,28 @@ import FoodItems from "./components/FoodItems";
 import "./App.css";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
-  let foodItems = ["Fruits", "Vegetables", "Rice", "Meat", "Egg", "Milk"];
-  const handleOnChange = (event: any) => {
-    console.log(event.target.value);
+  let [foodItems, setFoodItems] = useState([]);
+
+  const onKeyDown = (event: any) => {
+    if (event.key === "Enter") {
+      let newFoodItem = event.target.value;
+      event.target.value = "";
+      let newItems = [...foodItems, newFoodItem];
+      setFoodItems(newItems);
+    }
   };
 
   return (
     <>
       <Container>
-        <h1>Healthy Foods</h1>
+        <h1 className="food-heading">Healthy Foods</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
-        <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
-
-      {/* <Container>
-        <p>
-          Above is the list of healthy foods that are good for your health and
-          well being.
-        </p>
-      </Container> */}
     </>
   );
 }
--- 4-fragments/src/components/FoodInput.tsx
+++ 4-fragments/src/components/FoodInput.tsx
@@ -1,12 +1,12 @@
 import styles from "./FoodInput.module.css";
 
-const FoodInput = ({ handleOnChange }: any) => {
+const FoodInput = ({ handleKeyDown }: any) => {
   return (
     <input
       type="text"
       placeholder="Enter Food Item here"
       className={styles.foodInput}
-      onChange={handleOnChange}
+      onKeyDown={handleKeyDown}
     />
   );
 };

049b312662017cca37cd08a41480dec09de593e3
abdurRehman <Administrator@DESKTOP-GARLJAI>
2025-11-01 19:50:04 +0500

    minor changes for new project

--- projects/8-pakistan-clock-version-2/src/components/CurrentTime.tsx
+++ projects/8-pakistan-clock-version-2/src/components/CurrentTime.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+
 function CurrentTime() {
-  let time = new Date();
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <p className="lead">
       {" "}
-      This is the Current time : {time.toLocaleDateString()} -{" "}
-      {time.toLocaleTimeString()}{" "}
+      This is the Current time : {currentDate.toLocaleDateString()} -{" "}
+      {currentDate.toLocaleTimeString()}{" "}
     </p>
   );
 }
